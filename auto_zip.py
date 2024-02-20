686cdc6bc4a8990fc412ab61606653731c179dbb
Diego <101619179+Dieg0MV@users.noreply.github.com>
2024-02-20 12:24:31 -0600

    Update auto_zip.py

--- auto_zip.py
+++ auto_zip.py
@@ -1,16 +1,14 @@
 import gzip
 import os
 
-#directorio 
+
 folder_dow = "C:\\Users\\Sr Director Patricio\\Downloads"
-#extencion of file
+#extencion de los archivos 
 ext_file = ['.png', '.pdf', '.gif', '.txt', '.exe' ]
 
 #recorre el directorio 
 for files in os.listdir(folder_dow):
-    #Scroll through the list of extensions
     for i in  ext_file:
-        #Select files with the correct extension
         if files.endswith(i):
             
             with open(os.path.join(folder_dow, files), 'rb') as f_input:
